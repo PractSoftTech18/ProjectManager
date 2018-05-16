90568aba6a85dafc10d3ffe05a3272c7d71adf86
Julia Hofer <hofer-julia@aon.at>
2018-05-16 09:40:53 +0200

    Added Java Project

---
+++ Project Manager/src/HelloWorld.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * @author Julia
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
