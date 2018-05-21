64c065233a756c5dc16d6df9ca8f297d307284fe
LydiaGrillenberger <lydia.grillenberger@gmail.com>
2018-05-21 13:07:56 +0200

    add changes in initialize GUI

--- ProjectManager/src/ProjectManager.java
+++ ProjectManager/src/ProjectManager.java
@@ -1,8 +1,31 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 import project.*;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+import javax.swing.JTabbedPane;
+import javax.swing.JPanel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JList;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+
+import java.awt.Color;
+import javax.swing.AbstractListModel;
+import javax.swing.DefaultListModel;
+import java.awt.GridLayout;
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 /**
  * @author Lydia Grillenberger, Julia Hofer and Lukas Schiefermueller
@@ -11,7 +34,9 @@ import project.*;
 public class ProjectManager {
 
 	private JFrame frame;
+	private JTable table;
 
+	private ArrayList<Project> projects;
 	/**
 	 * Launch the application.
 	 */
@@ -32,6 +57,7 @@ public class ProjectManager {
 	 * Create the application.
 	 */
 	public ProjectManager() {
+		projects = new ArrayList<>(10);
 		initialize();
 	}
 
@@ -42,6 +68,36 @@ public class ProjectManager {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		frame.getContentPane().add(tabbedPane, BorderLayout.CENTER);
+		tabbedPane.setBounds(0, 0, frame.getWidth(), frame.getHeight());
+		
+		JPanel panel = new JPanel();
+		tabbedPane.addTab("New tab", null, panel, null);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[]{0, 0};
+		gbl_panel.rowHeights = new int[]{0, 0};
+		gbl_panel.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+		panel.setLayout(gbl_panel);
+		//JLabel lblOurLabel = new JLabel("l");
+		//scrollPane.add(lblOurLabel);
+		
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		GridBagConstraints gbc_scrollPane_1 = new GridBagConstraints();
+		gbc_scrollPane_1.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane_1.gridx = 0;
+		gbc_scrollPane_1.gridy = 0;
+		panel.add(scrollPane_1, gbc_scrollPane_1);
+		
+		System.out.println(projects.size());
+		for (int i = 0; i < 10; i++) {
+			JTextPane p = new JTextPane();
+			p.setText("Test" + Integer.toString(i));
+			scrollPane_1.add(p);
+		}
 	}
 
 }
