e7faf11e9136caad3855c3883942c0580bf60bd4
LydiaGrillenberger <lydia.grillenberger@gmail.com>
2018-05-21 10:05:58 +0200

    add GUI file

--- ProjectManager/src/ProjectManager.java
+++ ProjectManager/src/ProjectManager.java
@@ -1,23 +1,47 @@
-/**
- * @author Lydia Grillenberger, Julia Hofer and Lukas Schiefermüller
- * @version 1.0
- *
- */
+import java.awt.EventQueue;
 
-/*
-import project.Project;
-import project.Priority;
-*/
+import javax.swing.JFrame;
+
 import project.*;
 
+/**
+ * @author Lydia Grillenberger, Julia Hofer and Lukas Schiefermueller
+ * @version 1.0
+ */
 public class ProjectManager {
 
+	private JFrame frame;
+
 	/**
-	 * @param args
+	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hallo Mädels!");
-		System.out.println("Hello World!");
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ProjectManager window = new ProjectManager();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ProjectManager() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
