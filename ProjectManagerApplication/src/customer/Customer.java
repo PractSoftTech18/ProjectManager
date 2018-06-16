12d3cb896621d1cdbd139f7db06f7375de5638e9
LydiaGrillenberger <lydia.grillenberger@gmail.com>
2018-06-16 12:16:53 +0200

    add packages customer and project, add private field status in project

---
+++ ProjectManagerApplication/src/customer/Customer.java
@@ -0,0 +1,56 @@
+package customer;
+
+import java.util.ArrayList;
+
+/**
+ * In this class all details about a customer are stored: an arbitrary number of persons where one is a
+ * contact person.
+ * 
+ * @author Lukas Schiefermueller
+ * @version 1.00, June 26th 2018
+ */
+public class Customer {
+	private ArrayList<Person> persons;
+	private int contactPerson;
+
+	/**
+	 * @param persons
+	 * @param contactPerson
+	 */
+	public Customer(ArrayList<Person> persons, int contactPerson) {
+		//super();
+		this.persons = persons;
+		this.contactPerson = contactPerson;
+	}
+
+	/**
+	 * @return the persons
+	 */
+	public ArrayList<Person> getPersons() {
+		return persons;
+	}
+
+	/**
+	 * @param persons
+	 *            the persons to set
+	 */
+	public void setPersons(ArrayList<Person> persons) {
+		this.persons = persons;
+	}
+
+	/**
+	 * @return the contactPerson
+	 */
+	public String getContactPerson() {
+		return persons.get(contactPerson).getName();
+	}
+
+	/**
+	 * @param contactPerson
+	 *            the contactPerson to set
+	 */
+	public void setContactPerson(int contactPerson) {
+		this.contactPerson = contactPerson;
+	}
+
+}
---
+++ ProjectManagerApplication/src/project/Priority.java
@@ -0,0 +1,11 @@
+package project;
+
+/**
+ * The priority of a project or a task can be low, normal or high.
+ * 
+ * @author Lukas Schiefermueller
+ * @version 1.00, June 26th 2018
+ */
+public enum Priority {
+	LOW, NORMAL, HIGH
+}
