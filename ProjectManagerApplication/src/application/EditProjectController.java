fafb799b5f6f525c39f33aee77d4b189c6a698e8
LukasSchiefermueller <lukas.schiefermueller@aon.at>
2018-06-26 18:48:47 +0200

    import Project

--- ProjectManagerApplication/src/application/EditProjectController.java
+++ ProjectManagerApplication/src/application/EditProjectController.java
@@ -15,6 +15,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import project.Project;
 
 /**
  * controller for EditProject
